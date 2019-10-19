8d57074ec85effab977642743ef06baec1b01db5
sreetej1998 <41690429+sreetej1998@users.noreply.github.com>
2019-10-19 18:27:28 +0530

    red

--- messagePassingTCP/tcpserver.c
+++ messagePassingTCP/tcpserver.c
@@ -1,24 +1,15 @@
+ 
 #include	<stdio.h>
-
 #include	<stdlib.h>
-
-#include	<errno.h>
-
-#include	<string.h>
-
-#include	<sys/types.h>
-
-#include	<sys/socket.h>
-
-#include	<netinet/in.h>
-
-#include	<arpa/inet.h>
-
-#include	<sys/wait.h>
-
-#include	<fcntl.h>
-
 #include	<unistd.h>
+#include	<errno.h>
+#include	<string.h>
+#include	<sys/types.h>
+#include	<sys/stat.h>
+#include	<sys/socket.h>
+#include	<netinet/in.h>
+#include	<arpa/inet.h>
+#include	<fcntl.h>
 
 int main(int argc, char * argv[]) {
   int n, bindFd, socketFd, connectFd;
